a9443826b7635f9d76bb57f22ba14730d61e3935
lijovijayan - office <anoop.chandran@blueripples.com>
2020-02-24 21:57:04 +0530

    test username

--- app/views/main.tsx
+++ app/views/main.tsx
@@ -23,6 +23,7 @@ export class MainPage extends React.Component< Props ,State> {
                     <ProfilePage username='myName' />
                     <HomePage />
                 </ScrollView>
+                {/* fix error */}
                 <BottomNavBar activeTab={this.state.activeIndex} switchTabs={this.switchTabs} />
             </View>
         );
