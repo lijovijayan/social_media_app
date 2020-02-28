4be27f706f4d565ad87db86f2486b1d20822814a
lijovijayan <lijovijayan00@gmail.com>
2020-02-28 19:51:28 +0530

    home component - status bar

--- app/views/home.tsx
+++ app/views/home.tsx
@@ -1,6 +1,8 @@
 import { ScrollView } from 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 interface Props {
     navigation: any;
 }
@@ -17,7 +19,16 @@ export class HomePage extends React.Component<Props> {
                 style={{
                     backgroundColor: 'white',
                 }}>
-                <View style={ [styles.column]}>
+                <View style={[styles.column]}>
+                    <ScrollView style={styles.statusBar} horizontal={true}
+                        showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+                        <View style={[styles.statusIconContainer, { backgroundColor: "white" }]}>
+                            <View style={styles.addIconContainer}>
+                                <MaterialCommunityIcons style={{ color: "white" }} size={25} name={'plus'} />
+                            </View>
+                        </View>
+                        {this.renderImages()}
+                    </ScrollView>
                     <View style={styles.card} >
                         <Text style={styles.text}>Card</Text>
                     </View>
@@ -42,6 +53,21 @@ export class HomePage extends React.Component<Props> {
                 </View>
             </ScrollView>
         );
+    }
+    renderImages() {
+        let images = [];
+        for (let i = 0; i < 20; i++) {
+            images.push(<View
+                key={i}
+                style={styles.statusIconContainer}>
+                <Image
+                    key={i}
+                    style={styles.statusIcon}
+                    source={{ uri: `https://picsum.photos/${Math.floor(Math.random() * Math.floor(300) + 1000)}/50` }}
+                />
+            </View>);
+        }
+        return images;
     }
 }
 const styles = StyleSheet.create({
@@ -75,7 +101,46 @@ const styles = StyleSheet.create({
         elevation: 5,
         backgroundColor: "white",
     },
+    statusBar: {
+        width: "94%",
+        height: 80,
+        marginTop: 15,
+    },
     text: {
         textAlign: "center",
+    },
+    statusIconContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: 70,
+        width: 70,
+        borderRadius: 35,
+        backgroundColor: "white",
+        alignSelf: "center",
+        marginLeft: 5,
+        marginRight: 5,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowColor: "black",
+        elevation: 4,
+        borderWidth: 1,
+        borderColor: "#3c8ab8",
+        padding: 2,
+        overflow: "hidden",
+    },
+    statusIcon: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 35,
+    },
+    addIconContainer: {
+        backgroundColor: "#3c8ab8",
+        width: "100%",
+        height: "100%",
+        borderRadius: 35,
+        alignItems: "center",
+        justifyContent: "center",
     }
 });
